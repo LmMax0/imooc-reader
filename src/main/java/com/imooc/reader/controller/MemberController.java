9154d3e60193b97e4c09291aaebfd80d40ebe4f4
LmMax0 <3364196039@qq.com>
2022-02-19 17:31:41 +0800

    用户登录以及状态保存

--- src/main/java/com/imooc/reader/controller/MemberController.java
+++ src/main/java/com/imooc/reader/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.imooc.reader.controller;
 
+import com.imooc.reader.entity.Member;
 import com.imooc.reader.service.MemberService;
 import com.imooc.reader.service.exception.BussinessException;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +27,12 @@ public class MemberController {
     public ModelAndView showRegister(){
         return new ModelAndView("/register");
     }
+
+    @GetMapping("/login.html")
+    public ModelAndView showLogin(){
+        return new ModelAndView("/login");
+    }
+
     @PostMapping("/registe")
     @ResponseBody
     public Map registe(String vc, String username, String password, String nickname, HttpServletRequest request){
@@ -38,6 +46,31 @@ public class MemberController {
         }else{
             try {
                 memberService.createMember(username,password,nickname);
+                result.put("code","0");
+                result.put("msg","success");
+            }catch (BussinessException ex){
+                ex.printStackTrace();
+                result.put("code",ex.getCode());
+                result.put("msg",ex.getMsg());
+            }
+        }
+        return result;
+    }
+
+    @PostMapping("/check_login")
+    @ResponseBody
+    public Map checkLogin(String username, String password, String vc, HttpSession session, HttpServletRequest request){
+        // 正确验证码
+        String verifyCode = (String) request.getSession().getAttribute("kaptchaVerifyCode");
+        // 验证码对比(忽略大小写)
+        Map result = new HashMap();
+        if(vc == null || verifyCode == null || !vc.equalsIgnoreCase(verifyCode)){
+            result.put("code","VC01");
+            result.put("msg","验证码错误");
+        }else{
+            try{
+                Member member = memberService.checkLogin(username, password);
+                session.setAttribute("loginMember",member);
                 result.put("code","0");
                 result.put("msg","success");
             }catch (BussinessException ex){
